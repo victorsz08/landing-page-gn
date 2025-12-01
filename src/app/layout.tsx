2ee7f6afb044acf29b6149a6425226bb74afc50b
victorsz08 <joaovictorsal777@gmail.com>
2025-12-01 14:08:48 -0300

    fix: fix package json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter, Poppins } from "next/font/google";
 import { Layout } from "@/components/layout";
-import { Analytics } from "@vercel/analytics/next";
 
 const siteUrl =
     process.env.NEXT_PUBLIC_SITE_URL || "https://geraldonetotreinador.com.br";
@@ -70,7 +69,6 @@ export default function RootLayout({
                 >
                     <Layout>{children}</Layout>
                 </ThemeProvider>
-                <Analytics />
             </body>
         </html>
     );
